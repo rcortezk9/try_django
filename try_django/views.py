4271f9832fac52bc961cb8dfa24ccdea6004c194
Rene Cortez <rcortezk9@gmail.com>
2019-06-23 17:01:32 -0500

    Template Context Processors

--- try_django/views.py
+++ try_django/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 # Don't Repeat Yourself = DRY
 
 def home_page(request):
     my_title = "Hello there..."
+    context = {"title": my_title}
     # doc = "<h1>{title}</h1>".format(title=my_title)
     # django_rendered_doc = "<h1>{{title}}</h1>".format(title=my_title)
-    return render(request, "hello_world.html", {"title": my_title})
+    return render(request, "home.html", context)
 
 def about_page(request):
     return render(request, "about.html", {"title": "About Us"})
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = 'hello_world.html'
+    template_obj = get_template(template_name)
+    render_item = template_obj.render(context)
+    return HttpResponse(render_item)
