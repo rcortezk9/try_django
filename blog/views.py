87f7469833fdfc291fa801be5c8c9596ad6587df
Rene Cortez <rcortezk9@gmail.com>
2019-06-23 19:57:31 -0500

    Get object or 404

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
@@ -7,7 +6,7 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request, post_id):
-    obj = BlogPost.objects.get(id=post_id)
+    obj = get_object_or_404(BlogPost, id=post_id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
