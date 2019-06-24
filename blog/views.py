c0a02dce9cc50a7ec26fd3a62862905bcb03c941
Rene Cortez <rcortezk9@gmail.com>
2019-06-23 19:52:00 -0500

    Dynamic url base lookup

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,8 +6,8 @@ from .models import BlogPost
 
 
 
-def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+def blog_post_detail_page(request, post_id):
+    obj = BlogPost.objects.get(id=post_id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
--- try_django/urls.py
+++ try_django/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from blog.views import (
     blog_post_detail_page,
 )
@@ -28,9 +28,13 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
-    path('blog/', blog_post_detail_page),
-    path('about/', about_page),
-    path('contact/', contact_page),
+    path('blog/<int:post_id>/', blog_post_detail_page),
+    # re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page),
+    path('page/', about_page),
+    path('pages/', about_page),
+    re_path(r'^pages?/$', about_page),
+    re_path(r'^about/$', about_page),
     path('example/', example_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
