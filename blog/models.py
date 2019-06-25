ea76b0da692a15815ca57fda211504c383fd15b8
Rene Cortez <rcortezk9@gmail.com>
2019-06-24 22:08:50 -0500

    Blog post navigation

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,11 @@ class BlogPost(models.Model): # blogpost_set -> queryset
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
 
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"{self.get_absolute_url}/edit"
+
+    def get_delete_url(self):
+        return f"{self.get_absolute_url}/delete"
