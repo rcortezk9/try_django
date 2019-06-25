eba3348c6e3160b355eae65b9c73498c35abf926
Rene Cortez <rcortezk9@gmail.com>
2019-06-24 19:06:49 -0500

    Include URLs

--- try_django/urls.py
+++ try_django/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from blog.views import (
     blog_post_create_view,
-    blog_post_detail_view,
-    blog_post_list_view,
-    blog_post_update_view,
-    blog_post_delete_view,
 )
 
 from .views import (
@@ -32,11 +28,8 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
-    path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_view),
-    path('blog/<str:slug>/edit/', blog_post_update_view),
-    path('blog/<str:slug>/delete/', blog_post_delete_view),
     path('blog-new/', blog_post_create_view),
+    path('blog/', include('blog.urls')),
     # re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page),
     path('page/', about_page),
     path('pages/', about_page),
