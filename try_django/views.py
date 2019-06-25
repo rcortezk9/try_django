9f4a1143d435a1ad2722af554275c4a3e112d8ae
Rene Cortez <rcortezk9@gmail.com>
2019-06-24 19:36:47 -0500

    Submit Raw HTML Form

--- try_django/views.py
+++ try_django/views.py
@@ -18,7 +18,8 @@ def about_page(request):
 
 
 def contact_page(request):
-    return render(request, "hello_world.html", {"title": "Contact Us"})
+    print(request.POST)
+    return render(request, "form.html", {"title": "Contact Us"})
 
 
 
