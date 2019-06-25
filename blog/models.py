a1a957e3db47a3d82cddb7e6afcf5eb9cc809408
Rene Cortez <rcortezk9@gmail.com>
2019-06-24 21:13:14 -0500

    Associate Blog Post to a User

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
-class BlogPost(models.Model):
+User = settings.AUTH_USER_MODEL # A must do.
+
+class BlogPost(models.Model): # blogpost_set -> queryset
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
